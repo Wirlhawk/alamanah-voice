492797579f990ac52d05ded3977027baeceae7e9
Wirlhawk <rayhantogarlumbantobing@gmail.com>
2025-05-29 15:09:40 +0700

    minor chang (penting nya validasi form)

--- src/zod/schema.ts
+++ src/zod/schema.ts
@@ -1,9 +1,13 @@
 import { z } from "zod"
 
 const formSchema = z.object({
-  name: z.string().optional(),
+  name: z.string().max(20, {
+    message: "Nama tidak boleh lebih dari 20 karakter",
+  }).optional(),
   message: z.string().min(3, {
     message: "Form pesan harus di isi dengan minimal 3 karakter",
+  }).max(200, {
+    message: "Form pesan tidak boleh lebih dari 200 karakter",
   }),
 })
 
